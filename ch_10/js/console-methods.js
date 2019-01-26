405ab66b0f0ed9f8ecbae3f15a5f38187f85a6a4
Cam Utz <Dragon8029@gmail.com>
2019-01-26 08:28:45 -0700

    add console-methods.js

---
+++ ch_10/js/console-methods.js
@@ -0,0 +1,20 @@
+console.log('And we\'re off...'); // Info: script running
+
+var $form, width, height, area;
+$form = $('#calculator'); 
+
+$('form input[type="text"]').on('blur', function() { // When input loses focus
+    console.warn('You entered ', this.value ); // Warn: what was entered
+});
+
+$('#calculator').on('submit', function(e) { /// When the user clicks submit
+    e.preventDefault(); // Prevent the form submitting
+    
+    width = $('#width').val(); 
+    height = $('#height').val();
+
+    area = width * height; 
+    console.error(area); // Error: show area
+
+    $form.append('<p class="result">' + area + '</p>')
+});
