23bf21396b16515e48131e6c2c1b5357565e66d6
Cam Utz <Dragon8029@gmail.com>
2019-02-02 13:35:28 -0700

    add filter-tags.js

---
+++ ch_12/js/filter-tags.js
@@ -0,0 +1,22 @@
+(function() { 
+    var $imgs = $('#galler img'); // Store all images
+    var $buttons = $('#buttons'); // Store buttons element
+    var tagged = {}; // Create tagged object
+
+    $imgs.each(function(){ // Loop through images and
+        var img = this; // Store img in variable
+        var tags = $(this).data('tags'); // Get this element's tags
+
+        if (tags) { // If the element had tags
+            tags.split(',').forEach(function(tagName) { // Split at comma and 
+                if (tagged[tagName] == null) { // If object doesn't have tagg
+                    tagged[tagName] = []; // Add empty array to object
+                }
+                tagged[tagName].push(img); // Add the image to the array
+            });
+        }
+    });
+
+    // Buttons, event handlers, and filters go here
+    
+}());
