f43c168e5352796b03f7f7b9e8b6fc4f6e269ad7
Cam Utz <Dragon8029@gmail.com>
2019-02-03 07:45:17 -0700

    add Sortable Table Scipt to sort-table.js

--- ch_12/js/sort-table.js
+++ ch_12/js/sort-table.js
@@ -25,3 +25,37 @@ var compare = { // Declare compare object
         return a - b; // Return a minus b
     }
 };
+
+$('.sortable').each(function() { 
+    var $table = $(this); // This sortable table
+    var $tbody = $table.find('tbody'); // Store table body
+    var $controls = $table.find('th'); // Store table headers
+    var rows = $tbody.find('tr').toArray(); // Store array containing rows
+
+    $controls.on('click', function() { // When user clicks on a header
+        var $header = $(this); // Get the header
+        var order = $header.data('sort'); // Get value of data-sort attribute
+        var column; // Declare variable called column
+
+        // If selected item has ascending or descending class, reverse contents
+        if ($header.is('.ascending') || $header.is('.descending')) {
+            $header.toggleClass('ascending descending'); // Toggle to other class
+            $tbody.append(rows.reverse()); // Revers the array
+        } else { // Otherwise perform a sort
+            $header.addClass('ascending'); // Add class to header
+            // Remove asc or desc from all other headers
+            $header.siblings().removeClass('ascending descending'); 
+            if (compare.hasOwnProperty(order)) { // If compare object has method
+                column = $controls.index(this); // Search for column's index no
+
+                rows.sort(function(a, b) { // Call sort() on rows array
+                    a = $(a).find('td').eq(column).text(); // Get text of column in row a
+                    b = $(b).find('td').eq(column).text(); // Get text of column in row b
+                    return compare[order](a, b); // Call compare method
+                });
+
+                $tbody.append(rows);
+            }
+        }
+    });
+});
