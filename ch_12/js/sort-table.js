29d0a0c10cc876a7a38405b73cb7c8da7f3a8104
Cam Utz <Dragon8029@gmail.com>
2019-02-03 07:34:05 -0700

    add The Compare Object to sort-table.js

---
+++ ch_12/js/sort-table.js
@@ -0,0 +1,27 @@
+var compare = { // Declare compare object
+    name: function(a, b) { // Add a method called name
+        a = a.replace(/^the /i, ''); // Remove The from start of parameter
+        b = b.replace(/^the /i, ''); // Remove The from start of parameter
+
+        if (a < b) { // If value a is less than value b
+            return -1; // Return -1
+        } else { // Otherwise
+            return a > b ? 1 : 0; // If a is greater than b return 1 OR if they are the same return 0
+        }
+    },
+    duration: function(a, b) { // Add a method called duration
+        a = a.split(':'); // Split the time at the colon
+        b = b.split(':'); // Split the time at the colon
+
+        a = Number(a[0]) * 60 + Number(a[1]); // Convert the time to seconds
+        b = Number(b[0]) * 60 + Number(b[1]); // Convert the time to seconds
+
+        return a - b; // Return a minus b
+    },
+    date: function(a, b) { // Add a method called date
+        a = new Date(a); // New Date object to hold the date
+        b = new Date(b); // New Date object to hold the date
+
+        return a - b; // Return a minus b
+    }
+};
