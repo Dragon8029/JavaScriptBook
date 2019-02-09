1d87289597d0976a59e245175314ebe1064069d3
Cam Utz <Dragon8029@gmail.com>
2019-02-09 09:11:28 -0700

    add password-signup

---
+++ ch_13/js/password-signup.js
@@ -0,0 +1,31 @@
+(function () {
+    var password = document.getElementById('password'); // Store password inputs
+    var passwordConfirm = document.getElementById('conf-password'); 
+    function setErrorHighlighter(e) {
+        var target = e.target || e.srcElement; // Get target element
+        if (target.value.length < 8) { // If its length is < 8
+            target.className = 'fail'; // Set class to fail
+        } else { // Otherwise
+            target.className = 'pass'; // Set class to pass
+        }
+    }
+    function removeErrorHighlighter(e) {
+        var target = e.target || e.srcElement; // Get target element
+        if (target.className === 'fail') { // If class is fail
+            target.className = ''; // Clear class
+        }
+    }
+    function passwordsMatch(e) { 
+        var target = e.target || e.srcElement; // Get target element
+        // If value matches pwd and it is longer than 8 characters
+        if ((password.value === target.value) && target.value.length >= 8) {
+            target.className = 'pass'; // Set class to pass
+        } else { // Otherwise
+            target.className = 'fail'; // Set class to fail
+        }
+    }
+    addEvent(password, 'focus', remvoeErrorHighlighter); 
+    addEvent(password, 'blur', setErrorHighlighter); 
+    addEvent(passwordConfirm, 'focus', removeErrorHighlighter); 
+    addEvent(passwordConfirm, 'blur', passwordsMatch);
+}());
