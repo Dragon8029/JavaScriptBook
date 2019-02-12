85409070712805af334b5e9aabf923985c92f51e
Cam Utz <Dragon8029@gmail.com>
2019-02-11 19:16:51 -0700

    found another bug

--- ch_13/js/validation.js
+++ ch_13/js/validation.js
@@ -9,7 +9,7 @@
 
         // PERFORM GENERIC CHECKS (calls functions outside the event handler)
         var i;
-        for (i = 0, l = (elements.length - l); i < l; i++) {
+        for (i = 0, l = elements.length ; i < l; i++) {
             // Next line calls validateRequired() see p606 & validateTypes() p610
             isValid = validateRequired(elements[i]) && validateTypes(elements[i]);
             if (!isValid) { // If it does not pass these two tests
