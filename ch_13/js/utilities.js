99dc66541d0a8e841d80b37324938fdc9e5e9be0
Cam Utz <Dragon8029@gmail.com>
2019-02-12 18:49:53 -0700

    fix typo

--- ch_13/js/utilities.js
+++ ch_13/js/utilities.js
@@ -15,10 +15,10 @@ function addEvent(el, event, callback) {
 function removeEvent(el, event, callback) {
     if ('removeEventListener' in el) {
         // If removeEventListener works
-        el.removeEventListener(evetn, callback, false); // Use it
+        el.removeEventListener(event, callback, false); // Use it
     } else { 
         // Otherwise
-        el.detachEvent('on' + event, el [event + callback]); // Create IE fallback
+        el.detachEvent('on' + event, el[event + callback]); // Create IE fallback
         el[event + callback] = null; 
         el['e' + event + callback] = null; 
     }
